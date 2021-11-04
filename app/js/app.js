53fa95caff1af009116d437ce737706401d4bad7
wolfcreative <loonysnow@gmail.com>
2021-11-04 05:57:38 +0500

    add updates, write grid-system, rewrite css

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('js init!')
+    // JS-сетка
+    // Аналог UiKit3 Grid = https://getuikit.com/docs/grid
+    // 
+    // В чём фича: у нас есть сетка, у нас есть классы ширины которые зависимые от размера экрана.
+    // Вручную задавать margin для отступа сверху не получится из-за классов ширины. А значит нужно проставлять в нужный момент нужному блоку отступ сверху.
+    //
+    // Пока всё написано на jQuery, ибо с ним проще и быстрее. В будущем перепишу на чистый JS. Скорость обработки слишком очень важна.
+
+    function grid(){
+        let grids = $('[data-grid]');
+
+        $(grids).each(function( ) {
+            let blocks = $(this).children();
+            let arr = [];
+
+            $( blocks ).each(function( ) {
+                let $this = $(this);
+
+                let y = $this.position().top;
+            
+                let rowBlocks = $this;
+            
+                for (let i = $this.index() - 1; i >= 0; i--) {
+                    let block = blocks.eq(i);
+
+                    if (block.position().top == y) {
+                        rowBlocks = rowBlocks.add(block);
+                    } else {
+                        break;
+                    }
+                }
+                
+                for (let i = $this.index() + 1; i < blocks.length; i++) {
+                    let block = blocks.eq(i);
+                        
+                    if (block.position().top == y || y - block.position().top > 0) {
+                        rowBlocks = rowBlocks.add(block);
+                    } else {
+                        break;
+                    }
+                }
+
+                rowBlocks.addClass('grid-margin');
+                
+                arr.push(rowBlocks)
+            });
+
+            arr.forEach(function (value, i) {
+                if (i == 0){
+                    value.removeClass('grid-margin')
+                }
+            });
+        });
+    }
+
+    // Инициализация
+    window.onload = () => {
+        grid()
+    };
+    
+    // Ресайз окна
+    window.onresize = () => {
+        grid()
+    };
 })
