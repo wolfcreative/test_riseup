8b005a0a69d82b9aded4deaccd1545590d698ba0
wolfcreative <loonysnow@gmail.com>
2021-11-05 02:59:17 +0500

    add form inputs, add rangeJS, many changes

--- app/js/app.js
+++ app/js/app.js
@@ -6,22 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     // Вручную задавать margin для отступа сверху не получится из-за классов ширины. А значит нужно проставлять в нужный момент нужному блоку отступ сверху.
 
     function grid(){
-        let classes = document.querySelectorAll('.grid-margin')
+        let classes = [].slice.call(document.querySelectorAll('.grid-margin'))
+
         if (typeof classes !== undefined){
-            let classesArray = Object.keys(classes).map(e => classes[e])
-    
-            classesArray.forEach((el) => {
+            classes.forEach((el) => {
                 el.classList.remove('grid-margin');
             })
         }
 
-        let grids = document.querySelectorAll('[data-grid]')
-        let gridsArray = Object.keys(grids).map(e => grids[e])
-
-        gridsArray.forEach((grid) => {
+        let grids = [].slice.call(document.querySelectorAll('[data-grid]'))
+        
+        grids.forEach((grid) => {
             let blocks = grid.children
             
-            let blocksArray = Object.keys(blocks).map(e => blocks[e])
+            let blocksArray = [].slice.call(blocks)
             let arr = []
 
             blocksArray.forEach((block) => {
@@ -35,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     let element = blocks[i]
                         
                     if (element.offsetTop != y) {
-                        if (element.offsetTop - y > 10 || element.offsetTop - y < -10){ // Допустимая погрешность случай flex-middle
+                        if (element.offsetTop - y > 10 || element.offsetTop - y < -10){ // Допустимая погрешность на случай flex-middle
                             rowBlocks.push(element)
                         } else {
                             break
@@ -67,9 +65,65 @@ document.addEventListener('DOMContentLoaded', () => {
         return -1
     }
 
+    // input[type=range]
+    function commonInputRange(item) {
+        let value = item.value
+
+        let caption = ''
+        if (item.getAttribute('data-range-caption')){
+            caption = item.getAttribute('data-range-caption')
+        }
+
+        item.nextElementSibling.innerHTML = value + caption
+        
+        // Для хромоподобных
+        if(navigator.userAgent.indexOf('AppleWebKit') != -1){
+            var percent = Number(((value - item.getAttribute('min')) * 100) / (item.getAttribute('max') - item.getAttribute('min'))) + '%'
+
+            item.style.backgroundImage = `linear-gradient(to right, #2EB670 0%, #2EB670 ${percent}, #dbdcde ${percent}, #dbdcde 100%)`;
+        }
+    }
+
+    function initInputRange(){
+        let inputs = [].slice.call(document.querySelectorAll('input[type=range]'))
+
+        inputs.forEach((item) => {
+            item.classList.add('margin-remove')
+
+            let caption = ''
+            if (item.getAttribute('data-range-caption')){
+                caption = item.getAttribute('data-range-caption')
+            }
+
+            let value = item.value
+
+            let oldHTML = item.outerHTML
+            let newHTML = `
+                <div class="range-wrapper flex flex-middle" style="padding-top:7px">
+                    ${oldHTML}
+                    <div class="range-value margin-medium-left">${value}${caption}</div>
+                </div>
+            `;
+
+            item.outerHTML = newHTML;
+        })
+
+        let inputs2 = [].slice.call(document.querySelectorAll('input[type=range]'))
+        inputs2.forEach((item) => {
+            commonInputRange(item)
+
+            "input change".split(" ").forEach((evt) => 
+                item.addEventListener(evt, () => {
+                    commonInputRange(item)
+                })
+            );
+        })
+    }
+
     // Инициализация
     window.onload = () => {
         grid()
+        initInputRange()
     }
     
     // Ресайз окна
