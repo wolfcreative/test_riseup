a050c9ea8446712830a2b2504c9d0b8c73668961
wolfcreative <loonysnow@gmail.com>
2021-11-11 02:06:08 +0500

    fix gulp-inject, fix sticky offset for IE

--- app/js/app.js
+++ app/js/app.js
@@ -150,14 +150,14 @@ function commonInputRange(item) {
 
 // Поддержка position:sticky для IE
 function stickyForLegacy(){
-    if((navigator.userAgent.indexOf('MSIE')!==-1 || navigator.appVersion.indexOf('Trident/') > -1)){
+    if((navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > -1)){
         let nav = document.querySelector('.position-sticky');
         let offset = nav.getBoundingClientRect();
 
         ['scroll', 'resize'].forEach(function(e){
             window.addEventListener(e, () => {
                 if (window.innerWidth >= 960){
-                    if (window.pageYOffset > offset.top) {
+                    if (window.pageYOffset > offset.top - 34) {
                         nav.style.position = 'fixed';
                         nav.style.top = '24px';
                     } else {
